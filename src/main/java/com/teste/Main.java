00c2ebacfc667383013b60752b2985d8a22ab1d4
cleysonnn <you@example.com>
2019-11-25 13:15:23 -0300

    algo

--- src/main/java/com/teste/Main.java
+++ src/main/java/com/teste/Main.java
@@ -1,32 +1,33 @@
 package com.teste;
 
-
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Calculadora cal = new Calculadora();
-		
-		System.out.println("digite qual operaçao deseja");
-		Scanner leitor = new Scanner(System.in);
-		String resUser = leitor.nextLine();
-		int num = Integer.parseInt(leitor.nextLine());
-		int num2 = Integer.parseInt(leitor.nextLine());
-		if (resUser.equals("1")){
-			int resul =  cal.somar(num, num2);
-			System.out.println(resul);
-		}else if (resUser.equals("2")){
+		public static void main(String[] args) {
+			Calculadora cal = new Calculadora();
 			
-			int resul = cal.sub(num, num2);
-			System.out.println(resul);
-		}else if (resUser.equals("3")){
+			System.out.println("digite qual operaçao deseja");
+			Scanner leitor = new Scanner(System.in);
+			String resUser = leitor.nextLine();
+			int num = Integer.parseInt(leitor.nextLine());
+			int num2 = Integer.parseInt(leitor.nextLine());
+			if (resUser.equals("1")){
+				int resul =  cal.somar(num , num2);
+				System.out.println(resul);
+			}else if (resUser.equals("2")){
+				
+				int resul = cal.sub(num, num2);
+				System.out.println(resul);
+			}else if (resUser.equals("3")){
+				
+				int resul = cal.mult(num, num2);
+				System.out.println(resul);
+			}
 			
-			int resul = cal.mult(num, num2);
-			System.out.println(resul);
+			
+	leitor.close();
 		}
 		
-		
-leitor.close();
 	}
-	
-}
+
+
